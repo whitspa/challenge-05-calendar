553042a0a930360ddcfb31da1b26ce9253b4c17f
whit <whitspa64@gmail.com>
2022-10-31 12:09:16 -0400

    working on the acceptance criteria. outlining the design.

---
+++ script.js
@@ -0,0 +1,10 @@
+const today = moment();
+$("currentDay").text(today.format("MMM Do, YYYY"));
+
+// Why doesn't current day show up?
+// Use JQuery UI for Hourly layout
+// Do I have my JQuery libraries properly loaded/placed?
+// Do I have the correct JQuery libraries loaded?
+// How to implement/execute Jquery UI widgets that you like
+// calling the JQuery Fcn on to the HTML Element:
+// How to "Sketch In " the functionality now and then go back and edit,style etc. later
