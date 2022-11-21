ef02b819030ad9bd4347904ddfe5b95fd5c3fd98
whit <whitspa64@gmail.com>
2022-11-21 14:41:15 -0500

    added details to readme including screen shot. worked on local storage function.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# challenge-05-calendar Work Day SCheduler
+# challenge-05 Work Day SCheduler
+Hello! This repository contains the code I created for the fifth challenge:
+Work day Scheduler.
+I worked with HTML, CSS, Javascript, JQuery, and Moment.js to build this solution.
+Here is the link to the deployed application:
+ https://whitspa.github.io/challenge-05-calendar/
+
+And here is the link to the repository:
+https://github.com/whitspa/challenge-05-calendar.git
+
+
+My github repository is published under the MIT license
+and I can be reached via email at: whitspa64@gmail.com
+with any questions
+
+A screen shot of the deployed application is below
+
+
+<img src="work-day-scheduler-screen-shot.png"
+     alt="scheduler with time rows color coded grey past red present green future"
+     style="float: left; margin-right: 10px;" />
--- script.js
+++ script.js
@@ -1,92 +1,91 @@
 
-//save text to local storage
-//saved events persist after refresh
-
-const history = JSON.parse(localStorage.getItem('tasks')) || ["", "", "", "", "", "", "", "", ""];
-
-console.log(moment().isSame('13:00', 'hour'))
-console.log(moment().hour())
-console.log(moment()._d.getHours())
-console.log(typeof((moment()._d.getHours())))
 const today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-var times = []  
-for(i=9; i< 18;i++){ //This For-Loop dynamically creates the set of hours I will use in my scheduler
+var times = []
+for (i = 9; i < 18; i++) { //This For-Loop dynamically creates the set of hours I will use in my scheduler
     times.push(`${i}:00`)
 }
-console.log(times)
-
+function getStorage(index) {
+    console.log('getting storage for', index)
+    const history = JSON.parse(localStorage.getItem('tasks')) || ["", "", "", "", "", "", "", "", ""];
+    if (history[index]) {
+        console.log(history[index])
+        return history[index]
+    }
+    return ""
+}
 //var container = document.getElementById('container')
 var container = document.getElementsByClassName('container')[0]
-for(let i=0; i<times.length; i++){ //This For-Loop displays the schedule rows //
-container.innerHTML+= `<div class="row" data-index="${i}">
+for (let i = 0; i < times.length; i++) { //This For-Loop displays the schedule rows and looks for tasks in each row to save to storage.
+    container.innerHTML += `<div class="row" data-index="${i}">
     <div class="hour col-1">${times[i]}</div>
-    <textarea class="description col-9 ${getClass(times[i])}">${history[i]}</textarea>
+    <textarea class="description col-9 ${getClass(times[i])}">${getStorage(i)}</textarea>
     <button class="saveBtn col-2"> Save </button>
 </div>` //Bootstrap documentation calls for 12 colums per row in the grid system for containers.
- //So I designated the column width of hours, text area, and button proportionally to add up to 12
+    //So I designated the column width of hours, text area, and button proportionally to add up to 12
 }
 
- //This is my save to local storage fcn. 
+
+//This is my save to local storage fcn. 
 //Need help figuring out how to index  event listener to the position in the array
 //I'm thinking event.target previousSibling but I'm struggling with the logic and syntax
-//also 
 // const container =document.querySelector(".container");
 // container.addEventListener ("click", function(event)
 //     const element = event.target;
 // if (element.matches (".row")) {
-//     const description = this.previousSibling
+//     const description/tasks maybe = this.previousSibling
+//
+
 // }
 
 
-// function saveToStorage(description) {
-
-//console.log(history);
-// history.push(description)
-// localStorage.setItem('tasks', JSON.stringify(history))
-// }
+function saveToStorage(task, index) {
+    const history = JSON.parse(localStorage.getItem('tasks')) || ["", "", "", "", "", "", "", "", ""];
+    history[index] = task
+    localStorage.setItem('tasks', JSON.stringify(history))
+}
 
 //function renderTasks(){
+//const description/tasks = JSON.parse(localStorage.getItem("description/tasks"));
+//   if (description/tasks !== null) {
+//     document.querySelector(".message").textContent = description/tasks
+//}
 
-function getClass(time){  // this function returns the current time from Moment.js
+function getClass(time) {  // this function returns the current time from Moment.js
     var currentHour = moment()._d.getHours()
     time = (Number(time.split(":")[0]))
-    console.log(time < currentHour)
-    if (time < currentHour){ //This If-Then statement evaluates all the hours in the schedule against the current hour and dynamically styles the hours according to  past, present, and future 
+    if (time < currentHour) { //This If-Then statement evaluates all the hours in the schedule against the current hour and dynamically styles the hours according to  past, present, and future 
         return "past"
     }
-    if (time > currentHour){
+    if (time > currentHour) {
         return "future"
     }
     return "present"
 }
 
-    //Intrigued by the case structure but I couldn't debug it
-    // switch (time) { 
-    //     case time < currentHour:
-    //         return "past"
-    //         break;
-    //     case time > currentHour:
-    //         return "future"
-    //         break;
-    //     default:
-    //         return "present"
-    //         break;
-    // }
+//Intrigued by the case structure but I couldn't debug it
+// switch (time) { 
+//     case time < currentHour:
+//         return "past"
+//         break;
+//     case time > currentHour:
+//         return "future"
+//         break;
+//     default:
+//         return "present"
+//         break;
+// }
 
-$(".saveBtn").on("click",function(event) {
-  event.preventDefault();
-    
-  const description = 
-  document.getElementsByClassName("description").textContent;
-  
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    console.log(event.target.parentNode.children[1].value)
+    console.log(event.target.parentNode.getAttribute('data-index'))
+    saveToStorage(event.target.parentNode.children[1].value, event.target.parentNode.getAttribute('data-index'))
 })
 
-console.log("row");
 
-
-//saveToStorage()
+saveToStorage()
 
 
 
